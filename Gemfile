a0e4b4423fb3b0699b1d81d265f4be48e2ac803a
rd2 <denis@rd2.ca>
2023-01-01 07:37:40 -0500

    Copyright (2023) + OSv3.5.1

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source "https://rubygems.org"
 
+gem "oslg", git: "https://github.com/rd2/oslg", branch: "y2023"
+
 gemspec
--- lib/osut/version.rb
+++ lib/osut/version.rb
@@ -1,6 +1,6 @@
 # BSD 3-Clause License
 #
-# Copyright (c) 2022, Denis Bourgeois
+# Copyright (c) 2022-2023, Denis Bourgeois
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -29,5 +29,5 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 module OSut
-  VERSION = "0.2.7".freeze
+  VERSION = "0.2.8".freeze
 end
